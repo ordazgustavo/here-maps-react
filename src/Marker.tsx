46b0c6829bd8988226d6416461fa4391f4d59f7e
Gustavo Ordaz <ordazsgustavo@gmail.com>
2019-02-19 16:57:14 -0500

    feat: add support for marker drag event

--- src/Marker.tsx
+++ src/Marker.tsx
@@ -4,15 +4,41 @@ import ReactDOMServer from 'react-dom/server'
 import getDomMarkerIcon from './utils/get-dom-marker-icon'
 import getMarkerIcon from './utils/get-marker-icon'
 import MapContext, { HEREMapContext } from './utils/map-context'
+import { setMarkerDragEvent } from './utils/set-drag-event'
+
+const markerEvents = {
+  onDragStart: 'dragstart',
+  onDrag: 'drag',
+  onDragEnd: 'dragend',
+}
 
 export interface MarkerProps extends H.map.Marker.Options, H.geo.IPoint {
   bitmap?: string
+  draggable?: boolean
+  onDragStart?: (e: H.util.Event) => void
+  onDrag?: (e: H.util.Event) => void
+  onDragEnd?: (e: H.util.Event) => void
 }
 
 export class Marker extends React.Component<MarkerProps> {
   public static contextType = MapContext
   public context: HEREMapContext
-  private marker: H.map.DomMarker | H.map.Marker | null = null
+  private marker?: H.map.DomMarker | H.map.Marker
+
+  public componentDidMount() {
+    const { map } = this.context
+    if (map && !this.marker) {
+      this.addMarkerToMap()
+    }
+
+    if (this.marker) {
+      Object.entries(markerEvents).forEach(([event, hereEvent]) => {
+        if (typeof this.props[event] === 'function') {
+          this.marker!.addEventListener(hereEvent, this.props[event])
+        }
+      })
+    }
+  }
 
   public componentDidUpdate(prevProps: MarkerProps) {
     if (prevProps.lat !== this.props.lat || prevProps.lng !== this.props.lng) {
@@ -26,15 +52,22 @@ export class Marker extends React.Component<MarkerProps> {
   public componentWillUnmount() {
     const { map } = this.context
 
+    if (this.marker) {
+      Object.entries(markerEvents).forEach(([event, hereEvent]) => {
+        if (typeof this.props[event] === 'function') {
+          this.marker!.removeEventListener(hereEvent, this.props[event])
+        }
+      })
+    }
+
     if (map && this.marker) {
       map.removeObject(this.marker)
     }
   }
 
   private addMarkerToMap() {
-    const { map } = this.context
-
-    const { children, bitmap, lat, lng } = this.props
+    const { map, behavior } = this.context
+    const { children, bitmap, lat, lng, draggable } = this.props
 
     let marker: H.map.DomMarker | H.map.Marker
     if (map) {
@@ -42,24 +75,25 @@ export class Marker extends React.Component<MarkerProps> {
         const html = ReactDOMServer.renderToStaticMarkup(
           <div className="dom-marker">{children}</div>,
         )
-
         const icon = getDomMarkerIcon(html)
-
         marker = new H.map.DomMarker({ lat, lng }, { icon })
-        map.addObject(marker)
       } else if (bitmap) {
         const icon = getMarkerIcon(bitmap)
 
         marker = new H.map.Marker({ lat, lng }, { icon })
-        map.addObject(marker)
       } else {
         marker = new H.map.Marker({ lat, lng })
-        map.addObject(marker)
       }
 
+      if (draggable && behavior) {
+        marker.draggable = draggable
+        setMarkerDragEvent(map, behavior)
+      }
+
+      map.addObject(marker)
       this.marker = marker
     }
-    return <div />
+    return null
   }
 
   private setPosition(point: H.geo.IPoint): void {
@@ -69,12 +103,6 @@ export class Marker extends React.Component<MarkerProps> {
   }
 
   public render() {
-    const { map } = this.context
-
-    if (map && !this.marker) {
-      this.addMarkerToMap()
-    }
-
     return null
   }
 }
